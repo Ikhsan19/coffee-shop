35f0c9057339f92bcfe119a8ace04e9952b4569e
Muhammad Ikhsan <muh.ikhsan818@gmail.com>
2024-03-24 12:10:28 +0800

    style:  bg-color & opacity

--- src/app/components/hero/index.tsx
+++ src/app/components/hero/index.tsx
@@ -4,12 +4,13 @@ export default function Hero() {
     return (
         <section>
             <div className="h-screen bg-cover bg-bottom" style={{ backgroundImage: "url(/home/hero/hero.avif)" }}>
-                <div className="container mx-auto h-full flex items-center">
-                    <div className="w-full text-center lg:w-1/2 sm:w-full sm:text-center lg:text-left">
-                        <h1 className="text-white font-bold text-6xl mb-8">Indulge in the Aroma and Warmth of Coffee</h1>
-                        <p className="text-white mb-10">Welcome to a unique journey through captivating coffee aromas and flavors. Discover beauty in every sip and feel the comforting warmth of an inspiring cup of coffee.</p>
-                        <Link href="/products" className="bg-[#6F4E37] hover:bg-brown p-3 text-white">Explore Menu</Link>
-
+                <div className="h-full w-full flex items-center bg-black bg-opacity-40">
+                    <div className="container mx-auto">
+                        <div className="w-full text-center lg:w-1/2 sm:w-full sm:text-center lg:text-left">
+                            <h1 className="text-white font-bold text-6xl mb-8">Indulge in the Aroma and Warmth of Coffee</h1>
+                            <p className="text-white mb-10">Welcome to a unique journey through captivating coffee aromas and flavors. Discover beauty in every sip and feel the comforting warmth of an inspiring cup of coffee.</p>
+                            <Link href="/products" className="bg-[#6F4E37] hover:bg-brown p-3 text-white">Explore Menu</Link>
+                        </div>
                     </div>
                 </div>
             </div>
