e73dfafe2897d09f8d26499a2f7ee762dfbf2be0
Muhammad Ikhsan <muh.ikhsan818@gmail.com>
2024-05-29 16:00:48 +0800

    refactor: clean code

--- src/components/navigation/navbar/Logo.tsx
+++ src/components/navigation/navbar/Logo.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 const Logo = () => {
     return (
-        <>
+        <div>
             <Link href="/">
                 <h1 className="text-3xl lg:text-4xl md:py-1 lg:py-2 font-bold text-white italic"><span className="text-[#6F4E37]">Kahawa</span>Street</h1>
             </Link>
-        </>
+        </div>
     );
 };
 
